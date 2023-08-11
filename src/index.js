c40f068bbeebe2e19056ef1907298cab39943489
David Forbes <davidforbesi@hotmail.com>
2023-08-11 12:24:05 +0100

    added logic to bring up add to do form

--- src/index.js
+++ src/index.js
@@ -3,3 +3,19 @@ const now = new Date();
 const span = document.querySelector('#date');
 
 span.textContent = dateFormat(now, "dddd mmmm dS");
+
+const addTodoFormButton = document.querySelector('#add-todo-container');
+const addTodoForm = document.querySelector('#add-todo-form');
+const cancelForm = document.querySelector('#cancel-form')
+
+
+addTodoFormButton.addEventListener('click', () => {
+  addTodoForm.style.display = 'grid';
+  addTodoFormButton.style.display = 'none';
+})
+
+cancelForm.addEventListener('click', () => {
+  addTodoForm.style.display = 'none';
+  addTodoFormButton.style.display = 'block';
+})
+
