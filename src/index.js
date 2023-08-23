c2d93808030f434deedf999596df7d8f6215e658
David Forbes <davidforbesi@hotmail.com>
2023-08-23 13:44:30 +0100

    restructured todo list array functions

--- src/index.js
+++ src/index.js
@@ -5,99 +5,108 @@ const nextDay = new Date(now);
 nextDay.setDate(now.getDate() + 1);
 const tomorrow = dateFormat(nextDay, 'yyyy-mm-dd');
 
-const todoListArrayToday = [{ title: "Task for today", description: "something here", priority: "medium", dueDate: today, project: 'Inbox', completed: false },
-];
-const todoListArrayUpcoming = [{ title: "Task for tomorrow", description: "something here", priority: "medium", dueDate: tomorrow, project: 'Inbox', completed: false },
-{ title: "Task for next week", description: "something here", priority: "medium", dueDate: "2023-08-28", project: 'Inbox', completed: false }];
-
-const todoListCompleted = [];
-
-const project = ['Project', { title: 'Study Web Development', description: '', dueDate: '2023-08-25', priority: '', project: 'Project', completed: false }];
-
-const todoListArray = [todoListArrayToday, todoListArrayUpcoming, todoListCompleted, project];
-
-let currentPage = 'Inbox';
-
 const ui = UIFunctions();
+const todoArrayFunctions = TodoArrayFunctions();
 
 ui.eventListeners();
 ui.displayTodoList();
-todoListCounter();
+todoArrayFunctions.todoListCounter();
 
-const CreateTodo = function (title, description, dueDate, priority, project, completed) {
-  const todo = {};
-  todo.title = title;
-  todo.description = description;
-  todo.dueDate = dueDate;
-  todo.priority = priority
-  todo.project = project;
-  todo.completed = completed;
-  return todo;
-}
+function TodoArrayFunctions() {
+
+  const todoListArrayToday = [{ title: "Task for today", description: "something here", priority: "medium", dueDate: today, project: 'Inbox', completed: false },
+  ];
+  const todoListArrayUpcoming = [{ title: "Task for tomorrow", description: "something here", priority: "medium", dueDate: tomorrow, project: 'Inbox', completed: false },
+  { title: "Task for next week", description: "something here", priority: "medium", dueDate: "2023-08-28", project: 'Inbox', completed: false }];
+
+  const todoListCompleted = [];
+
+  const project = ['Project', { title: 'Study Web Development', description: '', dueDate: '2023-08-25', priority: '', project: 'Project', completed: false }];
+
+  const todoListArray = [todoListArrayToday, todoListArrayUpcoming, todoListCompleted, project];
+
+  const CreateTodo = function (title, description, dueDate, priority, project, completed) {
+    const todo = {};
+    todo.title = title;
+    todo.description = description;
+    todo.dueDate = dueDate;
+    todo.priority = priority
+    todo.project = project;
+    todo.completed = completed;
+    return todo;
+  }
 
 
-function addTodo(title, description, dueDate, priority, project) {
-  const newTodo = CreateTodo(title, description, dueDate, priority, project, false);
-  if (newTodo.dueDate == today && newTodo.project == 'Inbox') todoListArrayToday.push(newTodo);
-  if (newTodo.dueDate !== today && newTodo.project == 'Inbox') todoListArrayUpcoming.push(newTodo);
-  if (newTodo.dueDate !== today && newTodo.project !== 'Inbox') {
-    for (let i = 2; i < todoListArray.length; i++) {
-      if (todoListArray[i][0] == newTodo.project) {
-        todoListArray[i].push(newTodo);
+  function addTodo(title, description, dueDate, priority, project) {
+    const newTodo = CreateTodo(title, description, dueDate, priority, project, false);
+    if (newTodo.dueDate == today && newTodo.project == 'Inbox') todoListArrayToday.push(newTodo);
+    if (newTodo.dueDate !== today && newTodo.project == 'Inbox') todoListArrayUpcoming.push(newTodo);
+    if (newTodo.dueDate !== today && newTodo.project !== 'Inbox') {
+      for (let i = 2; i < todoListArray.length; i++) {
+        if (todoListArray[i][0] == newTodo.project) {
+          todoListArray[i].push(newTodo);
+        }
       }
     }
-  }
-  console.log(todoListArray);
-}
-
-function addProject(value) {
-  const newProject = [value];
-  todoListArray.push(newProject);
-  console.log(todoListArray);
-}
-
-function todoListCounter() {
-  let inboxCounter = 0;
-  let todayCounter = 0;
-  let upcomingCounter = 0;
-  let completedCounter = 0;
-  todoListArray.forEach(array => array.forEach((item) => {
-    if (typeof (item) == 'object' && item.completed == false) { inboxCounter++ }
-  }));
-  todoListArray.forEach(array => array.forEach((item) => {
-    if (item.dueDate == today && typeof (item) == 'object' && item.completed == false) todayCounter++
-  }));
-  todoListArray.forEach(array => array.forEach((item) => {
-    if (item.dueDate !== today && typeof (item) == 'object' && item.completed == false) upcomingCounter++
-  }));
-
-  todoListCompleted.forEach(() => completedCounter++);
-
-  for (let i = 3; i < todoListArray.length; i++) {
-    let counter = 0;
-    todoListArray[i].forEach(item => { if (typeof (item) == 'object') { counter++ } });
-    let project = todoListArray[i][0];
-    ui.displayNumberOfTodos(project, counter);
+    console.log(todoListArray);
   }
 
-  ui.displayNumberOfTodos('Inbox', inboxCounter);
-  ui.displayNumberOfTodos('Today', todayCounter);
-  ui.displayNumberOfTodos('Upcoming', upcomingCounter);
-  ui.displayNumberOfTodos('Completed', completedCounter);
+  function addProject(value) {
+    const newProject = [value];
+    todoListArray.push(newProject);
+    console.log(todoListArray);
+  }
+
+  function todoListCounter() {
+    let inboxCounter = 0;
+    let todayCounter = 0;
+    let upcomingCounter = 0;
+    let completedCounter = 0;
+    todoListArray.forEach(array => array.forEach((item) => {
+      if (typeof (item) == 'object' && item.completed == false) { inboxCounter++ }
+    }));
+    todoListArray.forEach(array => array.forEach((item) => {
+      if (item.dueDate == today && typeof (item) == 'object' && item.completed == false) todayCounter++
+    }));
+    todoListArray.forEach(array => array.forEach((item) => {
+      if (item.dueDate !== today && typeof (item) == 'object' && item.completed == false) upcomingCounter++
+    }));
+
+    todoListCompleted.forEach(() => completedCounter++);
+
+    for (let i = 3; i < todoListArray.length; i++) {
+      let counter = 0;
+      todoListArray[i].forEach(item => { if (typeof (item) == 'object') { counter++ } });
+      let project = todoListArray[i][0];
+      ui.displayNumberOfTodos(project, counter);
+    }
+
+    ui.displayNumberOfTodos('Inbox', inboxCounter);
+    ui.displayNumberOfTodos('Today', todayCounter);
+    ui.displayNumberOfTodos('Upcoming', upcomingCounter);
+    ui.displayNumberOfTodos('Completed', completedCounter);
+  }
+
+  function removeTodo(array, index) {
+    todoListArray[array].splice(index, 1);
+  }
+
+  function completeTodo(array, index) {
+    todoListArray[array][index].completed = true;
+    const completedTodo = todoListArray[array].splice(index, 1);
+    todoListCompleted.push(completedTodo[0]);
+    console.log(todoListArray);
+  }
+  return { addTodo, addProject, todoListCounter, removeTodo, completeTodo, todoListArray, todoListCompleted }
 }
 
-function removeTodo(array, index) {
-  todoListArray[array].splice(index, 1);
-}
 
-function completeTodo(array, index) {
-  todoListArray[array][index].completed = true;
-  const completedTodo = todoListArray[array].splice(index, 1);
-  todoListCompleted.push(completedTodo[0]);
-  console.log(todoListArray);
-}
 
 function UIFunctions() {
+
+  const todoArrayFunctions = TodoArrayFunctions();
+
+  let currentPage = 'Inbox';
 
   const addTodoFormButton = document.querySelector('.add-todo-container');
   const cancelTodoForm = document.querySelector('#cancel-form');
@@ -166,15 +175,15 @@ function UIFunctions() {
   function displayTodoList() {
     todoList.innerHTML = '';
     if (currentPage == 'Inbox') {
-      todoListArray.forEach(array => array.forEach((item, index) => { if (typeof (item) == 'object' && item.completed == false) displayTodo(item, index) }));
+      todoArrayFunctions.todoListArray.forEach(array => array.forEach((item, index) => { if (typeof (item) == 'object' && item.completed == false) displayTodo(item, index) }));
     } else if (currentPage == 'Today') {
-      todoListArray.forEach(array => array.forEach((item, index) => { if (item.dueDate == today && item.completed == false) displayTodo(item, index) }));
+      todoArrayFunctions.todoListArray.forEach(array => array.forEach((item, index) => { if (item.dueDate == today && item.completed == false) displayTodo(item, index) }));
     } else if (currentPage == 'Upcoming') {
-      todoListArray.forEach(array => array.forEach((item, index) => { if (item.dueDate !== today && typeof (item) == 'object' && item.completed == false) displayTodo(item, index) }));
+      todoArrayFunctions.todoListArray.forEach(array => array.forEach((item, index) => { if (item.dueDate !== today && typeof (item) == 'object' && item.completed == false) displayTodo(item, index) }));
     } else if (currentPage == 'Completed') {
-      todoListCompleted.forEach((item, index) => displayTodo(item, index));
+      todoArrayFunctions.todoListCompleted.forEach((item, index) => displayTodo(item, index));
     } else {
-      todoListArray.forEach(array => array.forEach((item, index) => { if (item.project == currentPage && typeof (item) == 'object' && item.completed == false) displayTodo(item, index) }));
+      todoArrayFunctions.todoListArray.forEach(array => array.forEach((item, index) => { if (item.project == currentPage && typeof (item) == 'object' && item.completed == false) displayTodo(item, index) }));
     }
   }
 
@@ -292,20 +301,20 @@ function UIFunctions() {
       console.log({ node, index })
       node.classList.add('line-through');
       if (node.classList.contains('today')) {
-        completeTodo(0, index);
+        todoArrayFunctions.completeTodo(0, index);
       } else if (node.classList.contains('upcoming')) {
-        completeTodo(1, index);
+        todoArrayFunctions.completeTodo(1, index);
       } else {
         const project = node.classList[0];
-        for (let i = 2; i < todoListArray.length; i++) {
-          if (todoListArray[i][0] == project) {
-            completeTodo(i, index)
+        for (let i = 2; i < todoArrayFunctions.todoListArray.length; i++) {
+          if (todoArrayFunctions.todoListArray[i][0] == project) {
+            todoArrayFunctions.completeTodo(i, index)
           }
         }
       }
       setTimeout(() => {
         displayTodoList();
-        todoListCounter();
+        todoArrayFunctions.todoListCounter();
       }, 1000);
     }
   }
@@ -315,20 +324,20 @@ function UIFunctions() {
       const node = e.target.parentElement.parentElement;
       const index = node.dataset.index;
       if (node.classList.contains('today')) {
-        removeTodo(0, index);
+        todoArrayFunctions.removeTodo(0, index);
       } else if (node.classList.contains('upcoming')) {
-        removeTodo(1, index);
+        todoArrayFunctions.removeTodo(1, index);
       } else {
         const project = node.classList[0];
-        for (let i = 2; i < todoListArray.length; i++) {
-          if (todoListArray[i][0] == project) {
-            removeTodo(i, index);
+        for (let i = 2; i < todoArrayFunctions.todoListArray.length; i++) {
+          if (todoArrayFunctions.todoListArray[i][0] == project) {
+            todoArrayFunctions.removeTodo(i, index);
           }
         }
       }
       displayTodoList();
-      todoListCounter();
-      console.log(todoListArray);
+      todoArrayFunctions.todoListCounter();
+      console.log(todoArrayFunctions.todoListArray);
     }
   }
 
@@ -338,17 +347,17 @@ function UIFunctions() {
     addProjectsFormButton.addEventListener('click', toggleProjectForm);
     cancelProjectsButton.addEventListener('click', toggleProjectForm);
     addTodoButton.addEventListener('click', () => {
-      addTodo(todoTitleInput.value,
+      todoArrayFunctions.addTodo(todoTitleInput.value,
         todoDescriptionInput.value,
         todoDueDateInput.value,
         todoPriorityInput.value,
         projectSelect.value);
       toggleTodoForm();
       displayTodoList();
-      todoListCounter();
+      todoArrayFunctions.todoListCounter();
     });
     addProjectsButton.addEventListener('click', () => {
-      addProject(addProjectsInput.value);
+      todoArrayFunctions.addProject(addProjectsInput.value);
       displayProject(addProjectsInput.value);
       toggleProjectForm();
     });
@@ -366,10 +375,4 @@ function UIFunctions() {
     displayTodoList,
     displayNumberOfTodos
   }
-}
-
-
-
-function todoFunctions() {
-
 }
